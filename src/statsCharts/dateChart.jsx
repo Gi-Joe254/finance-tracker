8c452aa229bf454d9a2617afc943e9cf98cbe676
Joe <jgacheru504@gmail.com>
2025-08-27 13:08:39 +0300

    added a line chart by date

--- src/statsCharts/dateChart.jsx
+++ src/statsCharts/dateChart.jsx
@@ -3,9 +3,9 @@ import { XAxis, YAxis, Tooltip, Cell, LineChart, Line } from "recharts";
 export default function DateChart({ data }) {
   const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042"]
   return (
-    <LineChart width={300} height={400} data={data}>
-      <Line type='monotone' dataKey='cat' stroke="red" />
-      <XAxis dataKey='amt' />
+    <LineChart width={400} height={300} data={data}>
+      <Line type='monotone' dataKey='amt' stroke="red" />
+      <XAxis dataKey='dat' />
       <YAxis />
       <Tooltip />
     </LineChart>
